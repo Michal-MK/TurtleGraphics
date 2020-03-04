eeb0d000e34557eca920fee01631bf26cfaea8f4
Michal Hazdra <469081@fi.muni.cz>
2020-03-04 11:36:54 +0100

    Initial commit - new rendering method?

---
+++ TurtleGraphics/XAML/MainWindowEmpty.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TurtleGraphics.XAML {
+	/// <summary>
+	/// Interaction logic for MainWindowEmpty.xaml
+	/// </summary>
+	public partial class MainWindowEmpty : Window {
+
+		public MainWindowEmpty() {
+			InitializeComponent();
+			//AddHandler(MouseDownEvent, new MouseButtonEventHandler(VisualParent_MouseDown), true);
+			AddHandler(MouseMoveEvent, new MouseEventHandler(MouseMoveEventHandle), true);
+		}
+
+		Point? prev;
+
+		private void MouseMoveEventHandle(object sender, MouseEventArgs e) {
+			Point p = e.GetPosition(this);
+			
+			if(prev == null) {
+				prev = p;
+				return;
+			}
+
+			Vector d = p - prev.Value;
+
+			if (e.LeftButton == MouseButtonState.Pressed) {
+				DrawingVisual v = CreateDrawingVisualRectangle(p.X, p.Y, d.X, d.Y, w: 2); 
+				visualHost.AddV(v);
+			}
+
+			prev = p;
+		}
+
+		private void VisualParent_MouseDown(object sender, MouseButtonEventArgs e) {
+			Point p = e.GetPosition(this);
+			DrawingVisual v = CreateDrawingVisualRectangle(p.X, p.Y, w:2); visualHost.AddV(v);
+		}
+
+		// Create a DrawingVisual that contains a rectangle.
+		private DrawingVisual CreateDrawingVisualRectangle(double x = 160, double y = 100, double dX = 320, double dY = 80, int w = 10) {
+			DrawingVisual drawingVisual = new DrawingVisual();
+
+			// Retrieve the DrawingContext in order to create new drawing content.
+			DrawingContext drawingContext = drawingVisual.RenderOpen();
+
+			Pen p = new Pen(Brushes.Black, w);
+
+			// Create a rectangle and draw it in the DrawingContext.
+			drawingContext.DrawLine(p,new Point(x,y), new Point(x + dX, y + dY));
+
+			// Persist the drawing content.
+			drawingContext.Close();
+
+			return drawingVisual;
+		}
+
+		private void visualHost_MouseDown(object sender, MouseButtonEventArgs e) {
+			DrawingVisual v = CreateDrawingVisualRectangle(); visualHost.AddV(v);
+		}
+
+		private void visualHost_PreviewMouseDown(object sender, MouseButtonEventArgs e) {
+			DrawingVisual v = CreateDrawingVisualRectangle(); visualHost.AddV(v);
+		}
+	}
+}
---
+++ TurtleGraphics/XAML/MyVisualHost.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace TurtleGraphics.XAML {
+	// Create a host visual derived from the FrameworkElement class.
+	// This class provides layout, event handling, and container support for
+	// the child visual objects.
+	public class MyVisualHost : FrameworkElement {
+		// Create a collection of child visual objects.
+		private VisualCollection _children;
+
+		public MyVisualHost() {
+			_children = new VisualCollection(this);
+
+			this.MouseLeftButtonUp += new System.Windows.Input.MouseButtonEventHandler(MyVisualHost_MouseLeftButtonUp);
+		}
+
+		public void AddV(DrawingVisual v) {
+			_children.Add(v);
+		}
+
+		// Provide a required override for the VisualChildrenCount property.
+		protected override int VisualChildrenCount {
+			get { return _children.Count; }
+		}
+
+		// Provide a required override for the GetVisualChild method.
+		protected override Visual GetVisualChild(int index) {
+			if (index < 0 || index >= _children.Count) {
+				throw new ArgumentOutOfRangeException();
+			}
+
+			return _children[index];
+		}
+
+		// Capture the mouse event and hit test the coordinate point value against
+		// the child visual objects.
+		void MyVisualHost_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+			// Retrieve the coordinates of the mouse button event.
+			System.Windows.Point pt = e.GetPosition((UIElement)sender);
+
+			// Initiate the hit test by setting up a hit test result callback method.
+			VisualTreeHelper.HitTest(this, null, new HitTestResultCallback(myCallback), new PointHitTestParameters(pt));
+		}
+
+		// If a child visual object is hit, toggle its opacity to visually indicate a hit.
+		public HitTestResultBehavior myCallback(HitTestResult result) {
+			if (result.VisualHit.GetType() == typeof(DrawingVisual)) {
+				if (((DrawingVisual)result.VisualHit).Opacity == 1.0) {
+					((DrawingVisual)result.VisualHit).Opacity = 0.4;
+				}
+				else {
+					((DrawingVisual)result.VisualHit).Opacity = 1.0;
+				}
+			}
+
+			// Stop the hit test enumeration of objects in the visual tree.
+			return HitTestResultBehavior.Stop;
+		}
+	}
+}
